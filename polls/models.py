180450135c694b6d1e1529c1da4dae7279fded7f
Joseph Bannon <jb9war@virginia.edu>
2021-09-16 17:16:34 -0400

    Added bootstrap elements

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,5 @@
-
 import datetime
-
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 
@@ -11,19 +10,29 @@ class Question(models.Model):
     
     def __str__(self):
         return self.question_text
-        
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
-    
+
+
 class Deepthought(models.Model):
     title_text = models.CharField('title_text',max_length=200, default="")
     thought_text = models.CharField('thought_text',max_length=200)
+
     def __str__(self):
         return self.title_text + ": " + self.thought_text
